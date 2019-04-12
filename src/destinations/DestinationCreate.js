7cf33131e7e3ebc57fa9cdfb59957be42f113d02
cpcurtis1218 <chuck.curtis.su@gmail.com>
2019-04-12 11:42:14 -0400

    Add component for create destination
    - Add form to enter location, contact, note
    - Add submit button
    - Add axios post request to /destinations
    - Add redirect back to destinations page on success

---
+++ src/destinations/DestinationCreate.js
@@ -0,0 +1,76 @@
+import React, { Component } from 'react'
+import axios from 'axios'
+import apiUrl from '../apiConfig'
+import { Redirect } from 'react-router'
+
+class DestinationCreate extends Component {
+  constructor () {
+    super()
+
+    this.state = {
+      destination: {
+        location: '',
+        contact: '',
+        note: ''
+      },
+      created: false
+    }
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault()
+
+    // destructuring the destination and props objects
+    const { destination } = this.state
+    const { user } = this.props
+
+    axios({
+      url: `${apiUrl}/destinations`,
+      method: 'post',
+      data: { destination },
+      headers: {
+        'Authorization': `Token token=${user.token}`
+      }
+    })
+      .then(response => this.setState({
+        created: true,
+        destination: response.data.destination }))
+      .catch(() => this.setState({
+        destination: { ...destination, location: '', contact: '', note: '' }
+      }),
+      console.log)
+  }
+
+  handleChange = (event) => {
+    console.log(event.target.name, event.target.value)
+
+    this.setState({ destination: {
+      ...this.state.destination,
+      [event.target.name]: event.target.value
+    } })
+  }
+
+  render () {
+    const { destination, created } = this.state
+
+    if (created) {
+      return <Redirect to="/destinations" />
+    } else {
+      return (
+        <div>
+          <form onSubmit={this.handleSubmit}>
+            <label htmlFor="location">Location</label>
+            <input value={destination.location} name="location" className="m-1" onChange={this.handleChange} />
+            <label htmlFor="contact">Contact</label>
+            <input value={destination.contact} name="contact" className="m-1" onChange={this.handleChange} />
+            <label htmlFor="note">Note</label>
+            <input value={destination.note} name="note" className="m-1" onChange={this.handleChange} />
+            <button type="submit">Submit</button>
+          </form>
+        </div>
+      )
+    }
+  }
+}
+
+export default DestinationCreate
