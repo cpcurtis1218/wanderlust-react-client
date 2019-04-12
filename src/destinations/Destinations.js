89113ed0c2b8b8041c5ed89b1de69495b09fa485
cpcurtis1218 <chuck.curtis.su@gmail.com>
2019-04-12 10:27:34 -0400

    Add folder and files for Destination and Destinations components

---
+++ src/destinations/Destinations.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+class Destinations extends Component {
+  constructor () {
+    super()
+
+    this.state = {
+      destinations: []
+    }
+  }
+
+  render () {
+    return (
+      <h4>Destinations: </h4>
+    )
+  }
+}
+export default Destinations
